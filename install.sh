79111f5e75f40f8f102a86eeaa7d1b9cf7d4e429
He <hegeh123@outlook.com>
2023-01-09 17:38:26 +0200

    Fix install script to work without kwin replace

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 if [[ `kpackagetool5 -l | grep "flexGrid"` ]]; then
     echo "Updating..."
     kpackagetool5 --type=KWin/Script -u .
+    kwriteconfig5 --file kwinrc --group Plugins --key flexGridEnabled false
+    qdbus org.kde.KWin /KWin reconfigure
+    sleep 1;
+    kwriteconfig5 --file kwinrc --group Plugins --key flexGridEnabled true
+    qdbus org.kde.KWin /KWin reconfigure
 else
     echo "Installing..."
     kpackagetool5 --type=KWin/Script -i .
